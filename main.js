76082ecc031f443a64f5bf28d07802ada86af5f0
Zachary Hahn <zackhahn98@gmail.com>
2023-04-17 21:35:33 -0700

    created pet cards

--- main.js
+++ main.js
@@ -240,3 +240,22 @@ const pets = [
       imageUrl: "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119"
     }
   ];
+
+const targetingApp =  document.querySelector("#app");
+console.log(targetingApp);
+
+let domString = "";
+for (const pet of pets){
+  domString += `<div class="card" style="width: 18rem;">
+  <img src=${pet.imageUrl} class="card-img-top" alt=${pet.name}>
+  <div class="card-body">
+    <h5 class="card-title">${pet.name}</h5>
+    <p>Type: ${pet.type}</p>
+    <p class="card-text">${pet.specialSkill}</p>
+    <p>Type: ${pet.type}</p>
+    <p>Color: ${pet.color}</p>
+  </div>
+</div>`;
+}
+
+targetingApp.innerHTML = domString;
